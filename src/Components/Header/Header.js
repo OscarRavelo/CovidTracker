46b2811c62b63d8570855319350522d74b2d6658
Oscar Ravelo <oscar91ravelo@gmail.com>
2020-11-06 19:35:23 +0100

    startin the proyect header done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import './Header.css';
+
+const Header = () => (
+    <header>
+    <div>
+
+        <h1>covid tracker by Oscar Ravelo</h1>
+    </div>
+
+    </header>
+)
+
+export default Header;
